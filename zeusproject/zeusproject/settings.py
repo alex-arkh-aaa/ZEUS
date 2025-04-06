91b6216addaa7d5ede5fd02cb1ce5c138a17da7b
alex-arkh-aaa <alex.arkhangelskiy@yandex.ru>
2025-04-07 00:26:26 +0300

    verstka login, auth and register forms

--- zeusproject/zeusproject/settings.py
+++ zeusproject/zeusproject/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'zeus_app/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
