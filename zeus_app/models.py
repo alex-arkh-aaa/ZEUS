72f3f8447925e84d93bd805d79bcfb32d4f82810
alex-arkh-aaa <alex.arkhangelskiy@yandex.ru>
2025-04-07 20:37:01 +0300

    models booking and comments were made. Already in database

--- zeus_app/models.py
+++ zeus_app/models.py
@@ -3,10 +3,20 @@ from wsgiref.validate import validator
 from django.core.validators import MinLengthValidator
 from django.db import models
 from django.core.exceptions import ValidationError
-
+from users.models import User
 # Create your models here.
 
-class User(models.Model):
-    name = models.CharField(max_length=20),
-    last_name = models.CharField(max_length=20),
-    
+
+class Booking(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    court_number = models.IntegerField(choices=[(1, 'Желтый корт № 1'), (2, 'Синий корт № 2'), (3, 'Оранжевый корт № 3')])
+    bookind_datetime = models.DateTimeField()
+
+
+
+
+
+class Comment(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=-1)
+    rating_value = models.IntegerField(choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')])
+    text = models.TextField(help_text='Напишите свой комментарий, пожалуйста!', )
